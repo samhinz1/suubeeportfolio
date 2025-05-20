f1bbe128db078b9126ae13f4b2626f547f6bb4ba
Sam Hinz <samhinz@live.com>
2025-05-20 11:47:02 +1000

    footer links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -24,14 +24,18 @@ export default function Footer() {
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="mr-4">
                 <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
               </svg>
-              <span>0424 639 003</span>
+              <Link href="tel:0424639003" className="hover:text-mint">
+                <span>0424 639 003</span>
+              </Link>
             </p>
             <p className="text-gray-400 text-sm flex items-center gap-1">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="mr-4">
                 <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
                 <polyline points="22,6 12,13 2,6"></polyline>
               </svg>
-              <span>info@suubee.com</span>
+              <Link href="mailto:info@suubee.com" className="hover:text-mint">
+                <span>info@suubee.com</span>
+              </Link>
             </p>
           </div>
 
