e916fac10be529d0eb3ebe78532cd04fafa6ef07
Sam Hinz <samhinz@live.com>
2025-09-19 14:56:45 +0100

    fix footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -86,7 +86,7 @@ export default function Footer() {
           <div>
             <h3 className="font-semibold mb-3 md:mb-4 text-gray-300">Connect With Us</h3>
             <div className="flex gap-4 mb-4 md:mb-6">
-              <Link href="https://www.linkedin.com/company/waimak-asset-management" className="text-gray-400 hover:text-mint" target="_blank" rel="noopener noreferrer">
+              <Link href="https://www.linkedin.com/company/suubee" className="text-gray-400 hover:text-mint" target="_blank" rel="noopener noreferrer">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                   <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
                   <rect x="2" y="9" width="4" height="12"></rect>
